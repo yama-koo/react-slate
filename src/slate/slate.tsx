6efbb97b82099dc704788198797c57432d6a4b1e
yama-koo <yamako.ilc@gmail.com>
2020-02-20 15:51:28 +0900

    markdown shortcut

--- src/slate/slate.tsx
+++ src/slate/slate.tsx
@@ -1,6 +1,14 @@
 import React, { useMemo, useState, useCallback } from 'react'
-import { createEditor, Node, Transforms, Editor, Text } from 'slate'
-import { withReact, Editable, Slate, RenderElementProps, RenderLeafProps } from 'slate-react'
+import { createEditor, Node, Transforms, Editor, Text, Range, Point } from 'slate'
+import {
+  withReact,
+  Editable,
+  Slate,
+  RenderElementProps,
+  RenderLeafProps,
+  ReactEditor,
+} from 'slate-react'
+import { withHistory } from 'slate-history'
 
 const CustomEditor = {
   isBoldMarkActive(editor: Editor) {
@@ -68,7 +76,7 @@ const CustomEditor = {
 }
 
 export const MySlate = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
+  const editor = useMemo(() => withShortcuts(withReact(withHistory(createEditor()))), [])
 
   const [value, setValue] = useState<Node[]>([
     {
@@ -85,6 +93,8 @@ export const MySlate = () => {
         return <ListElement {...props} />
       case 'ul':
         return <UlElement {...props} />
+      case 'heading-one':
+        return <h1 {...props.attributes}>{props.children}</h1>
       default:
         return <DefaultElement {...props} />
     }
@@ -191,4 +201,85 @@ const ListElement = (props: RenderElementProps) => {
 const UlElement = (props: RenderElementProps) => {
   const { attributes, children } = props
   return <ol {...attributes}>{children}</ol>
+}
+
+const SHORTCUTS: { [key: string]: string } = {
+  '*': 'list-item',
+  '-': 'list-item',
+  '+': 'list-item',
+  '>': 'block-quote',
+  '#': 'heading-one',
+  '##': 'heading-two',
+  '###': 'heading-three',
+  '####': 'heading-four',
+  '#####': 'heading-five',
+  '######': 'heading-six',
+}
+
+const withShortcuts = (editor: ReactEditor) => {
+  const { deleteBackward, insertText } = editor
+
+  editor.insertText = text => {
+    const { selection } = editor
+
+    if (text === ' ' && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection
+      const block = Editor.above(editor, {
+        match: n => Editor.isBlock(editor, n),
+      })
+      const path = block ? block[1] : []
+      const start = Editor.start(editor, path)
+      const range = { anchor, focus: start }
+      const beforeText = Editor.string(editor, range)
+      const type = SHORTCUTS[beforeText]
+
+      if (type) {
+        Transforms.select(editor, range)
+        Transforms.delete(editor)
+        Transforms.setNodes(editor, { type }, { match: n => Editor.isBlock(editor, n) })
+
+        if (type === 'list-item') {
+          const list = { type: 'bulleted-list', children: [] }
+          Transforms.wrapNodes(editor, list, {
+            match: n => n.type === 'list-item',
+          })
+        }
+
+        return
+      }
+    }
+
+    insertText(text)
+  }
+
+  editor.deleteBackward = (...args) => {
+    const { selection } = editor
+
+    if (selection && Range.isCollapsed(selection)) {
+      const match = Editor.above(editor, {
+        match: n => Editor.isBlock(editor, n),
+      })
+
+      if (match) {
+        const [block, path] = match
+        const start = Editor.start(editor, path)
+
+        if (block.type !== 'paragraph' && Point.equals(selection.anchor, start)) {
+          Transforms.setNodes(editor, { type: 'paragraph' })
+
+          if (block.type === 'list-item') {
+            Transforms.unwrapNodes(editor, {
+              match: n => n.type === 'bulleted-list',
+            })
+          }
+
+          return
+        }
+      }
+
+      deleteBackward(...args)
+    }
+  }
+
+  return editor
 }
